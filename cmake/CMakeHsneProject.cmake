2dcc8f112a898f4899281f1f5c5da2977b9cf53e
Soumyadeep Basu <44787782+basusoumyadeep@users.noreply.github.com>
2025-02-20 17:21:23 +0100

    Temporary changes for cmake and build

--- cmake/CMakeHsneProject.cmake
+++ cmake/CMakeHsneProject.cmake
@@ -2,18 +2,18 @@
 # HSNE Plugin Target
 # -----------------------------------------------------------------------------
 set(HSNE_PLUGIN "HsneAnalysisPlugin")
-
+ 
 # -----------------------------------------------------------------------------
 # Source files
 # -----------------------------------------------------------------------------
 add_subdirectory(src/HSNE)
-
+ 
 source_group(Common FILES ${COMMON_TSNE_SOURCES})
 source_group(Common//Actions FILES ${COMMON_ACTIONS_SOURCES})
 source_group(Actions FILES ${HSNE_ACTIONS_SOURCES})
 source_group(Hsne FILES ${HSNE_PLUGIN_SOURCES})
 source_group(Utils FILES ${THIRD_PARTY_JSON})
-
+ 
 # -----------------------------------------------------------------------------
 # CMake Target
 # -----------------------------------------------------------------------------
@@ -25,53 +25,64 @@ add_library(${HSNE_PLUGIN} SHARED
     ${HSNE_PLUGIN_SOURCES}
     ${THIRD_PARTY_JSON}
 )
-
+ 
 # -----------------------------------------------------------------------------
 # Target include directories
 # -----------------------------------------------------------------------------
 target_include_directories(${HSNE_PLUGIN} PRIVATE "${ManiVault_INCLUDE_DIR}")
 target_include_directories(${HSNE_PLUGIN} PRIVATE "src/Common")
 target_include_directories(${HSNE_PLUGIN} PRIVATE "third_party/json")
-
+ 
 set_HDILib_project_includes(${HSNE_PLUGIN})
-
+ 
 # -----------------------------------------------------------------------------
 # Target properties
 # -----------------------------------------------------------------------------
 # Request C++17, in order to use std::for_each_n with std::execution::par_unseq.
 set_target_properties(${HSNE_PLUGIN} PROPERTIES CXX_STANDARD 17)
-
+ 
 target_compile_definitions(${HSNE_PLUGIN} PRIVATE QT_MESSAGELOGCONTEXT)
-
+ 
 if(MV_UNITY_BUILD)
     set_target_properties(${HSNE_PLUGIN} PROPERTIES UNITY_BUILD ON)
 endif()
-
+ 
 # -----------------------------------------------------------------------------
 # Target library linking
 # -----------------------------------------------------------------------------
 target_link_libraries(${HSNE_PLUGIN} PRIVATE Qt6::Widgets)
 target_link_libraries(${HSNE_PLUGIN} PRIVATE Qt6::WebEngineWidgets)
-
+ 
 target_link_libraries(${HSNE_PLUGIN} PRIVATE ManiVault::Core)
 target_link_libraries(${HSNE_PLUGIN} PRIVATE ManiVault::PointData)
 target_link_libraries(${HSNE_PLUGIN} PRIVATE ManiVault::ImageData)
-
+ 
 target_link_libraries(${HSNE_PLUGIN} PRIVATE ${OPENGL_LIBRARIES})
-
+ 
+# Find OpenCV
+find_package(OpenCV REQUIRED)
+ 
+# Check if OpenCV is found
+if(OpenCV_FOUND)
+    message(STATUS "OpenCV found: ${OpenCV_VERSION}")
+    target_link_libraries(${HSNE_PLUGIN} PRIVATE ${OpenCV_LIBS})
+else()
+    message(FATAL_ERROR "OpenCV not found!")
+endif()
+ 
 if(OpenMP_CXX_FOUND)
     target_link_libraries(${HSNE_PLUGIN} PRIVATE OpenMP::OpenMP_CXX)
 endif()
-
+ 
 set_flann_project_link_libraries(${HSNE_PLUGIN})
 set_HDILib_project_link_libraries(${HSNE_PLUGIN})
-set_lz4_project_link_libraries(${HSNE_PLUGIN}) 
-
+set_lz4_project_link_libraries(${HSNE_PLUGIN})
+ 
 set_optimization_level(${HSNE_PLUGIN} ${MV_SNE_OPTIMIZATION_LEVEL})
 mv_check_and_set_AVX(${HSNE_PLUGIN} ${MV_SNE_USE_AVX})
-
+ 
 silence_opengl_deprecation(${HSNE_PLUGIN})
-
+ 
 # -----------------------------------------------------------------------------
 # Target installation
 # -----------------------------------------------------------------------------
@@ -79,7 +90,7 @@ install(TARGETS ${HSNE_PLUGIN}
     RUNTIME DESTINATION Plugins COMPONENT PLUGIN_HSNE # Windows .dll
     LIBRARY DESTINATION Plugins COMPONENT PLUGIN_HSNE # Linux/Mac .so
 )
-
+ 
 if (NOT DEFINED ENV{CI})
     add_custom_command(TARGET ${HSNE_PLUGIN} POST_BUILD
         COMMAND "${CMAKE_COMMAND}"
@@ -90,12 +101,12 @@ if (NOT DEFINED ENV{CI})
         --verbose
     )
 endif()
-
+ 
 set_target_properties(${HSNE_PLUGIN}
     PROPERTIES
     FOLDER AnalysisPlugins
 )
-
+ 
 # -----------------------------------------------------------------------------
 # Misc
 # -----------------------------------------------------------------------------
